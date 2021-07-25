9cdab3d0f262c4ba959e72ab5cd17e88e83eebbe
AlexRatik <aleksandr-ratkevich@mail.ru>
2021-07-25 14:11:40 +0300

    feat add solutinon in src

--- src/index.js
+++ src/index.js
@@ -4,9 +4,26 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    let open_b = 0;
+    let close_b = 0;
+    for (let item of expr) {
+        if (item === "(") {
+            open_b++;
+        }
+        if (item === ")") {
+            close_b++;
+        }
+    }
+    let div_by_zero = expr.match(/\/ 0/, "");
+    if (div_by_zero) {
+        throw new Error("TypeError: Division by zero.");
+    }
+    if (open_b !== close_b) {
+        throw new Error("ExpressionError: Brackets must be paired");
+    }
+    return new Function(`return ${expr}`)();
 }
 
 module.exports = {
-    expressionCalculator
-}
+    expressionCalculator,
+};
